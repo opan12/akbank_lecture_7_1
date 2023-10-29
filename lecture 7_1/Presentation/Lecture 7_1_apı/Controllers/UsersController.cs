0673f467650436ccf38d1df28c5b8ac9acec7a2f
SUDE NUR OPAN <114245040+opan12@users.noreply.github.com>
2023-10-29 16:20:28 +0300

    [Feat]Sloution Created

---
+++ lecture 7_1/Presentation/Lecture 7_1_apı/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Week_7_1.Domain.Entites; 
+using Week_7_1.Persistence.Contexts;
+
+namespace Week_7_1.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly Week_7_1DbContext _context;
+
+        public UsersController()
+        {
+            _context = new();
+        }
+
+
+        [HttpGet]
+        public List<User> GetAll()
+        {
+            return _context.Users.ToList();
+        }
+    }
+}
---
+++ lecture 7_1/Presentation/Lecture 7_1_apı/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Lecture_7_1_apı.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
